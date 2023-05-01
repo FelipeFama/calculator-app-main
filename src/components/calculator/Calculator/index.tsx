480feab77e802d6d3ff5b0ce97a64b02aff4ab76
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-05-01 01:38:30 -0300

    Feat: add calculate feature

--- src/components/calculator/Calculator/index.tsx
+++ src/components/calculator/Calculator/index.tsx
@@ -10,11 +10,71 @@ export default function CalculatorComponent() {
   const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
   const ref = useRef<HTMLDivElement>(null);
 
+  const updateCalc = (value: string) => {
+    const displayValue = ref.current;
+
+    if (value === "0" && calc === "0") {
+      return clear;
+    } else if (
+      (operations.includes(value) && calc === "") ||
+      (operations.includes(value) && operations.includes(calc.slice(-1)))
+    ) {
+      return;
+    } else if (!operations.includes(value)) {
+      setCalc((+calc + +value).toString());
+
+      if (calc.length > 7) {
+        displayValue!.style.fontSize = "2.5rem";
+      }
+      if (calc.length === 11 && !operations.includes(value)) {
+        displayValue!.style.width = "fit-content";
+        displayValue!.style.overflow = "hidden";
+        return console.log(setCalc(calc.toString()));
+      } else if (calc.length === 11 && operations.includes(value)) {
+        console.log("hello");
+      }
+    }
+
+    if (calculated === true && operations.includes(value)) {
+      setCalculated(false);
+    }
+  };
+
+  const calculate = () => {
+    if (eval(calc) === undefined) {
+      return;
+    }
+    setCalculated(true);
+    setCalc(eval(calc).toString());
+  };
+
+  const clear = () => {
+    setCalculated(false);
+    setCalc("");
+  };
+
+  const del = () => {
+    if (calc === "") {
+      return;
+    }
+    const displayValue = ref.current;
+    if (calc.length < 7) {
+      displayValue!.style.fontSize = "3.3rem";
+    }
+    const value = calc.slice(0, -1);
+    setCalc(value);
+  };
+
   return (
     <main>
       <Calculator>
-        <Screen />
-        <Keypad />
+        <Screen ref={ref} calc={calc} />
+        <Keypad
+          updateCalc={updateCalc}
+          calculate={calculate}
+          del={del}
+          clear={clear}
+        />
       </Calculator>
     </main>
   );
--- src/components/calculator/Keypad/index.tsx
+++ src/components/calculator/Keypad/index.tsx
@@ -7,27 +7,143 @@ import {
   ResetKey,
 } from "./styles";
 
-export function Keypad() {
+interface KeypadProps {
+  updateCalc: (value: string) => void;
+  del: () => void;
+  clear: () => void;
+  calculate: () => void;
+}
+
+export const Keypad = (props: KeypadProps) => {
   return (
     <PressKeys>
-      <NumberKeys value="7">7</NumberKeys>
-      <NumberKeys value="8">8</NumberKeys>
-      <NumberKeys value="9">9</NumberKeys>
+      <NumberKeys
+        value="7"
+        onClick={() => {
+          props.updateCalc("7");
+        }}
+      >
+        7
+      </NumberKeys>
+      <NumberKeys
+        value="8"
+        onClick={() => {
+          props.updateCalc("8");
+        }}
+      >
+        8
+      </NumberKeys>
+      <NumberKeys
+        value="9"
+        onClick={() => {
+          props.updateCalc("9");
+        }}
+      >
+        9
+      </NumberKeys>
       <DeleteKey>DEL</DeleteKey>
-      <NumberKeys value="4">4</NumberKeys>
-      <NumberKeys value="5">5</NumberKeys>
-      <NumberKeys value="6">6</NumberKeys>
-      <OperatorKeys value="+">+</OperatorKeys>
-      <NumberKeys value="1">1</NumberKeys>
-      <NumberKeys value="2">2</NumberKeys>
-      <NumberKeys value="3">3</NumberKeys>
-      <OperatorKeys value="-">-</OperatorKeys>
-      <NumberKeys value=".">.</NumberKeys>
-      <NumberKeys value="0">0</NumberKeys>
-      <OperatorKeys value="/">/</OperatorKeys>
-      <OperatorKeys value="x">x</OperatorKeys>
-      <ResetKey value="reset">RESET</ResetKey>
-      <EqualsKey value="=">=</EqualsKey>
+      <NumberKeys
+        value="4"
+        onClick={() => {
+          props.updateCalc("4");
+        }}
+      >
+        4
+      </NumberKeys>
+      <NumberKeys
+        value="5"
+        onClick={() => {
+          props.updateCalc("5");
+        }}
+      >
+        5
+      </NumberKeys>
+      <NumberKeys
+        value="6"
+        onClick={() => {
+          props.updateCalc("6");
+        }}
+      >
+        6
+      </NumberKeys>
+      <OperatorKeys
+        value="+"
+        onClick={() => {
+          props.updateCalc("+");
+        }}
+      >
+        +
+      </OperatorKeys>
+      <NumberKeys
+        value="1"
+        onClick={() => {
+          props.updateCalc("1");
+        }}
+      >
+        1
+      </NumberKeys>
+      <NumberKeys
+        value="2"
+        onClick={() => {
+          props.updateCalc("2");
+        }}
+      >
+        2
+      </NumberKeys>
+      <NumberKeys
+        value="3"
+        onClick={() => {
+          props.updateCalc("3");
+        }}
+      >
+        3
+      </NumberKeys>
+      <OperatorKeys
+        value="-"
+        onClick={() => {
+          props.updateCalc("-");
+        }}
+      >
+        -
+      </OperatorKeys>
+      <NumberKeys
+        value="."
+        onClick={() => {
+          props.updateCalc(".");
+        }}
+      >
+        .
+      </NumberKeys>
+      <NumberKeys
+        value="0"
+        onClick={() => {
+          props.updateCalc("0");
+        }}
+      >
+        0
+      </NumberKeys>
+      <OperatorKeys
+        value="/"
+        onClick={() => {
+          props.updateCalc("/");
+        }}
+      >
+        /
+      </OperatorKeys>
+      <OperatorKeys
+        value="x"
+        onClick={() => {
+          props.updateCalc("*");
+        }}
+      >
+        x
+      </OperatorKeys>
+      <ResetKey value="reset" onClick={props.clear}>
+        RESET
+      </ResetKey>
+      <EqualsKey value="=" onClick={props.calculate}>
+        =
+      </EqualsKey>
     </PressKeys>
   );
-}
+};
