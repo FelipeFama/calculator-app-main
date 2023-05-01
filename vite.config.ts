f04e8c1f278bd3e38359f26415014bdb4ad7cf84
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-05-01 15:25:01 -0300

    Update project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "calculator",
+        short_name: "calculator app",
+        start_url: "/",
+        display: "standalone",
+        lang: "en",
+        scope: "/",
+        icons: [
+          {
+            src: "/favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
