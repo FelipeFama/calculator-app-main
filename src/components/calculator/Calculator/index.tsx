c701888d7b5262c005c56d29bc9dac6c57cb4ea4
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-04-30 21:01:51 -0300

    Feat: starting to add features

--- src/components/calculator/Calculator/index.tsx
+++ src/components/calculator/Calculator/index.tsx
@@ -1,8 +1,15 @@
 import { Calculator } from "./styles";
 import { Screen } from "../Screen";
 import { Keypad } from "../Keypad";
+import { useRef, useState } from "react";
 
 export default function CalculatorComponent() {
+  const [calc, setCalc] = useState<string>("");
+  const [calculated, setCalculated] = useState<boolean>(false);
+  const operations = ["/", "*", "+", "-", "."];
+  const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  const ref = useRef<HTMLDivElement>(null);
+
   return (
     <main>
       <Calculator>
