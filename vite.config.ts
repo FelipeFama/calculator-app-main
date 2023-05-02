32d9ddbf9e56b626731ad844365e12a36a1546be
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-05-01 23:36:30 -0300

    Update project

--- vite.config.ts
+++ vite.config.ts
@@ -7,19 +7,28 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      devOptions: { enabled: true },
       manifest: {
         name: "calculator",
         short_name: "calculator app",
+        description: "calculator app challenge",
+        theme_color: "#fff",
         start_url: "/",
         display: "standalone",
         lang: "en",
         scope: "/",
         icons: [
           {
-            src: "/favicon-32x32.png",
-            sizes: "32x32",
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
             type: "image/png",
-            purpose: "any maskable",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
           },
         ],
       },
